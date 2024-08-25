4c12a8c7900d0ce0f77eb40c52981c516d3158cc
halcyon-past <titanssuperior@gmail.com>
2024-08-25 12:36:22 +0530

    changed url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
       }
       setError('');
       
-      const res = await fetch('http://localhost:3000/bfhl', {
+      const res = await fetch('https://ec2-13-201-54-155.ap-south-1.compute.amazonaws.com/:3000/bfhl', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
